31033b14a3a0311b62f97920de37a31233ed40e2
minwoo <minwoo0904@naver.com>
2021-05-13 15:04:03 +0900

    0513 1차 수행  git, hub 추가

---
+++ 0513/src/TestExam.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class TestExam {
+
+	public static void main(String[] args) {
+		
+		int i, a, b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("500이하의 자연수를 입력하세요.");
+		
+		for(i=0; i<= 500; i++){
+			
+		}
+		if(i%3==0){
+			System.out.println();
+		}
+		else if (i%7==0)
+		System.out.println();
+		
+
+	}
+
+}
